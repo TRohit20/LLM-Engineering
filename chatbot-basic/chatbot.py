41aa66558b7ad3650451972b51f9af865e138d84
Rohit <vadlamudi.rohit5@gmail.com>
2024-01-29 00:13:31 +0530

    add LLM, setup chatbot responses

--- chatbot-basic/chatbot.py
+++ chatbot-basic/chatbot.py
@@ -1,4 +1,10 @@
 import streamlit as st 
+import openai
+
+# openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 st.title("Personal Chat Assistant")
 
@@ -14,7 +20,18 @@ if prompt := st.chat_input("What's it you want this time, mate?"):
         st.markdown(prompt)
     st.session_state.messages.append({"role":"user", "content": prompt})
 
-    response = f"Friendo: {prompt}"
     with st.chat_message("assistant"):
-        st.markdown(response)
-    st.session_state.messages.append({"role":"assistant", "content": response})
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model = st.session_state["openai_model"],
+            messages = [
+                { "role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream = True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "  ")
+        message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role":"assistant","content": full_response})
