a122023dd5f2eb54c91aef4360f52ae0fb43624a
Rohit <vadlamudi.rohit5@gmail.com>
2024-01-29 00:17:34 +0530

    chore: add comments

--- chatbot-basic/chatbot.py
+++ chatbot-basic/chatbot.py
@@ -1,28 +1,36 @@
 import streamlit as st 
 import openai
 
-# openai.api_key = st.secrets["OPENAI_API_KEY"]
+# Connect OPENAI API KEY to access LLM 
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 st.title("Personal Chat Assistant")
 
+# Initialise chat history 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# React to User Input
 if prompt := st.chat_input("What's it you want this time, mate?"):
+    # Displaying user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
+    # Add user message to chat history
     st.session_state.messages.append({"role":"user", "content": prompt})
 
+# System respone to the user prompt
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
+        # call the LLM to fetch response to the query 
         for response in openai.ChatCompletion.create(
             model = st.session_state["openai_model"],
             messages = [
