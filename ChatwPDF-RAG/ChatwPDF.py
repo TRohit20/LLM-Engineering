261a9ed208fef2ee3390d24b290a64fe9b658df6
Rohit <vadlamudi.rohit5@gmail.com>
2024-01-29 09:34:14 +0530

    feat: Support multiple filetypes & add comments

--- ChatwPDF-RAG/ChatwPDF.py
+++ ChatwPDF-RAG/ChatwPDF.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import streamlit as st
 import openai
 from langchain.chains import RetrievalQA
@@ -14,6 +15,7 @@ from io import BytesIO
 from typing import Tuple, List
 import pickle
 
+# Function to parse PDF and extract text
 def parse_pdf(file: BytesIO, filename: str) -> Tuple[List[str], str]:
     pdf = PdfReader(file)
     output = []
@@ -25,7 +27,7 @@ def parse_pdf(file: BytesIO, filename: str) -> Tuple[List[str], str]:
         output.append(text)
     return output, filename
 
-
+# Function to convert text to Document objects
 def text_to_docs(text: List[str], filename: str) -> List[Document]:
     if isinstance(text, str):
         text = [text]
@@ -50,12 +52,12 @@ def text_to_docs(text: List[str], filename: str) -> List[Document]:
             doc_chunks.append(doc)
     return doc_chunks
 
-
+# Function to create index from Document objects
 def docs_to_index(docs, openai_api_key):
     index = FAISS.from_documents(docs, OpenAIEmbeddings(openai_api_key=openai_api_key))
     return index
 
-
+# Function to create index for PDF files
 def get_index_for_pdf(pdf_files, pdf_names, openai_api_key):
     documents = []
     for pdf_file, pdf_name in zip(pdf_files, pdf_names):
@@ -67,10 +69,10 @@ def get_index_for_pdf(pdf_files, pdf_names, openai_api_key):
 # Set the title for the Streamlit app
 st.title("RAG enhanced Chatbot")
 
-# Set up the OpenAI API key from databutton secrets
+# Set up the OpenAI API key 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Cached function to create a vectordb for the provided PDF files
+# Cached function to create a vectordb for the provided files
 @st.cache_data
 def create_vectordb(files, filenames):
     # Show a spinner while creating the vectordb
@@ -81,8 +83,8 @@ def create_vectordb(files, filenames):
     return vectordb
 
 
-# Upload PDF files using Streamlit's file uploader
-pdf_files = st.file_uploader("", type="pdf", accept_multiple_files=True)
+# Upload files using Streamlit's file uploader
+pdf_files = st.file_uploader("", type="pdf, csv, txt, doc", accept_multiple_files=True)
 
 # If PDF files are uploaded, create the vectordb and store it in the session state
 if pdf_files:
